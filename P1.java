0dc4c7c1ba4fbfc7222a69b121255e6dc67db411
ritesh2403 <ritesh.ghorui@ia.ooo>
2019-09-11 13:38:56 +0530

    mul loop added

--- P1.java
+++ P1.java
@@ -3,15 +3,18 @@ class P1
  public static void main(String[] args)
   {
     int a,b,sum,mul,div;
+    int i=1; 
     a=10;
     b=20
     sum=a+b;
     System.out.println("sum is:"+sum); 
 
-
-   mul=a*b;
+   while(i<13)
+{
+   mul=2*i;
    System.out.println("mul");
-
+   i++
+}
    div=b/a;
    System.out.println("div");
 
